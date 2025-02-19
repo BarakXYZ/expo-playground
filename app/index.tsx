504ad1566ca0044737ab0f40645ef8d1ded6947f
BarakXYZ <iBarakXYZ@gmail.com>
2025-02-19 20:50:33 +0200

    Using StyleSheet in react native and manipulating some UI elements.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={Styles.container}>
+      <Text style={Styles.text}>
+        Hello World!
+      </Text>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: "white",
+    fontSize: 48,
+  }
+});
